a3959bf65d1da49fdb973b71ca1f2cd71840421b
Iulian <iulian_dragan@yahoo.com>
2020-05-18 10:09:05 +0200

    fix onload

--- R/z_onLoad.R
+++ R/z_onLoad.R
@@ -1,5 +1,5 @@
 .onLoad <- function(...){
-  options(hidden.fields = c(getOption('hidden.fields'),'SUBJID', 'USUBJID'))
-
+  options(hidden.fields = unique(c(getOption('hidden.fields'),'SUBJID', 'USUBJID')))
+  options(by.col = unique(c(getOption('by.col'),'SUBJID')))
   dsSwissKnife:::.init()
 }
