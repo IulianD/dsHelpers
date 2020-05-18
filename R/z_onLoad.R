64f8f2a8cbf311ffcbe0c058735e8e32fcacb362
Iulian <iulian_dragan@yahoo.com>
2020-05-18 09:49:21 +0200

    onLoad set hidden fields

--- R/z_onLoad.R
+++ R/z_onLoad.R
@@ -1,6 +1,5 @@
 .onLoad <- function(...){
-  if(is.null(getOption('hidden.fields'))){
-    options(hidden.fields = c(getOption('hidden.fields'),'SUBJID', 'USUBJID'))
-  }
+  options(hidden.fields = c(getOption('hidden.fields'),'SUBJID', 'USUBJID'))
+
   dsSwissKnife:::.init()
 }
